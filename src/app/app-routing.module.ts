7a5b916411a2d371e873cff407151625796f5143
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-03-30 19:15:23 +0530

    App routing updated.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { GlobalComponent } from './global/global.component';
 
 
 const routes: Routes = [
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'global',
+    component: GlobalComponent,
   },
 ];
 
