e1b839485536b1975acb398e28ca6be3cb9c9909
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-03-30 19:15:47 +0530

    App component updated.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
+  ngOnInit(): void {}
   title = 'covid';
 }
