206c13dce6843b0f16c3422abf7c2e4a6e717ab5
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-03-31 15:54:59 +0530

    creator credentiial added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
+import { AleartDialogComponent } from '../aleart-dialog/aleart-dialog.component';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  providers: [MatDialog]
 })
+
 export class HeaderComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+    private dialog: MatDialog,
+    public dialogRef: MatDialogRef<AleartDialogComponent>) {}
+    message = {
+      'title' : "TextError Technologies",
+      'address' : "Bengaluru",
+      'desingBy': "Design By : Harshal Raverkar & Shefali Sen",
+      'Web':"https://hraverkar.netlify.com"
+      }
 
   ngOnInit(): void {}
 
@@ -19,5 +32,15 @@ export class HeaderComponent implements OnInit {
   }
   onNewsClick(){
     this.router.navigate(['news'], {});
+  }
+  
+  onClickfab(){
+    let dialogRef = this.dialog.open(AleartDialogComponent, {
+      width: '30%',
+      height: '30%',
+      data: this.message
+    });
+    dialogRef.afterClosed().subscribe(result => {
+    });
   }
 }
